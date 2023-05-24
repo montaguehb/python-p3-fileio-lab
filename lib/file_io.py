1b000f66ca3980efcb0c85229fba6c1a10b3223e
Historia Montague <historia.montague@outlook.com>
2023-05-24 14:56:18 -0600

    Finished Lab

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,15 @@
 def write_file(file_name, file_content):
-    pass
+    with open(f"{file_name}.txt", "w", encoding="utf-8")  as file:
+        file.write(file_content)
+        file.close()
+
 
 def append_file(file_name, append_content):
-    pass
+    with open(f"{file_name}.txt", "a", encoding="utf-8") as file:
+        file.write(append_content)
+        file.close()
+
 
 def read_file(file_name):
-    pass
+    with open(f"{file_name}.txt", 'r', encoding="utf-8") as file:
+        return file.read()
